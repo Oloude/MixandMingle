90b94d2843b93820a2549526ca773de950a7a2f0
Abosede Oloude Oyamine <oloudeabosede26@gmail.com>
2026-07-20 17:42:39 +0100

    added link

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,8 +1,10 @@
+import { useNavigate } from "react-router";
+
 function Hero() {
+  const navigate = useNavigate();
   return (
     <section className="flex flex-col items-center justify-center gap-6 bg-black/30 h-200 bg-hero">
-      <div className="flex flex-col gap-3 items-center " >
-        
+      <div className="flex flex-col gap-3 items-center ">
         <h1 className="font-raleway text-white font-bold text-5xl uppercase tracking-[2.6px]">
           Mix & mingle
         </h1>
@@ -11,9 +13,13 @@ function Hero() {
         </p>
       </div>
 
-      <button className="w-35 h-10 border border-white text-white text-[11px] uppercase tracking-[1.65px] hover:bg-black hover:border-black transition-all">
+      <a
+        href="https://wa.me/2348077182884"
+        target="_blank"
+        className="w-35 h-10 flex items-center justify-center border border-white text-white text-[11px] uppercase tracking-[1.65px] hover:bg-black hover:border-black transition-all"
+      >
         View Gallery
-      </button>
+      </a>
     </section>
   );
 }
--- src/ImagesContainer.jsx
+++ src/ImagesContainer.jsx
@@ -1,10 +1,11 @@
 import { PiShareFatLight } from "react-icons/pi";
 import { TfiDownload } from "react-icons/tfi";
-import { Link } from "react-router";
+import { Link, useNavigate } from "react-router";
 
 const numbers = Array.from({ length: 21 }, (_, index) => index + 1);
 
 function ImagesContainer() {
+  const navigate = useNavigate();
   return (
     <section className="px-5 sm:px-25 py-10 flex flex-col gap-10 ">
       <div className="flex items-center justify-between">
@@ -28,13 +29,20 @@ function ImagesContainer() {
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
         {numbers.map((i) => (
           <Link key={i} to={`img/${i}`} className="bg-black/10 h-50">
-            <img src={`/imgs/img_${i}.jpg`} className="w-full h-full object-cover object-center" />
+            <img
+              src={`/imgs/img_${i}.jpg`}
+              className="w-full h-full object-cover object-center"
+            />
           </Link>
         ))}
       </div>
-      <button className="w-35 h-10 self-center bg-black text-white text-[11px] uppercase tracking-[1.65px] hover:border hover:text-black hover:bg-white cursor-pointer hover:border-black transition-all">
+      <a
+        href="https://wa.me/2348077182884"
+        target="_blank"
+        className="w-35 h-10 self-center flex items-center justify-center bg-black text-white text-[11px] uppercase tracking-[1.65px] hover:border hover:text-black hover:bg-white cursor-pointer hover:border-black transition-all"
+      >
         View More
-      </button>
+      </a>
     </section>
   );
 }
